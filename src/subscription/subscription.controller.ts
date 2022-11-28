d8424e7093d5473bfecfb36a90303aa516d6172a
TubesForLyfe <13520160@std.stei.itb.ac.id>
2022-11-28 13:10:28 +0700

    Feature: Approve or Reject Subscription API

--- src/subscription/subscription.controller.ts
+++ src/subscription/subscription.controller.ts
@@ -15,7 +15,6 @@ export class SubscriptionController {
             try {
                 const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET_KEY);
                 if (decodedToken.isAdmin) {
-                    let data = "";
                     await axios.post(`${process.env.BINOTIFY_SOAP_API}/subscription`, 
                         '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sub="http://subscription.binotify/">' +
                             '<soapenv:Header/>' +
@@ -31,8 +30,38 @@ export class SubscriptionController {
                             'Content-Type': 'text/xml'
                         }
                     }).then(response => {
-                        data = xml2json(response.data, { spaces: 2, compact: true });
-                        res.send(data);
+                        res.send(xml2json(response.data, { spaces: 2, compact: true }));
+                    })}
+            } catch(e) {}
+        }
+        return await Promise.resolve({"error": "Unauthorized"});
+    }
+
+    @Post('accept_or_reject')
+    async AcceptOrRejectSubscriptionList(@Req() req: any, @Body() subscription: any, @Res() res: any) {
+        if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+            const token: string = req.headers.authorization.split(' ')[1];
+            try {
+                const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET_KEY);
+                if (decodedToken.isAdmin) {
+                    await axios.post(`${process.env.BINOTIFY_SOAP_API}/subscription`, 
+                        '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sub="http://subscription.binotify/">' +
+                            '<soapenv:Header/>' +
+                            '<soapenv:Body>' +
+                            '<sub:approveOrRejectSubscription>' +    
+                                '<request>' +
+                                    '<creatorId>' + subscription.creatorId + '</creatorId>' +
+                                    '<subscriberId>' + subscription.subscriberId + '</subscriberId>' +
+                                    '<approve>' + subscription.approve + '</approve>' +
+                                '</request>' +
+                            '</sub:approveOrRejectSubscription>' +
+                            '</soapenv:Body>' +
+                        '</soapenv:Envelope>', {
+                        headers: {
+                            'Content-Type': 'text/xml'
+                        }
+                    }).then(response => {
+                        res.send(xml2json(response.data, { spaces: 2, compact: true }));
                     })}
             } catch(e) {}
         }
