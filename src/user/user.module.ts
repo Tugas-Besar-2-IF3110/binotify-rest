44824cfb96221c06fbbc62f61b77a01579a1ef9a
TubesForLyfe <13520160@std.stei.itb.ac.id>
2022-12-02 10:28:02 +0700

    Feature: Caching

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), CacheModule.register()],
   controllers: [UserController],
   providers: [UserService]
 })
